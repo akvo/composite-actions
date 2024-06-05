ee893ed01d24cdd0a0651cb16d5f183d7ef2f7ad
Anjar Fiandrianto <anjar@akvo.org>
2024-06-05 13:15:02 +0700

    [#2] Add docker compose file location parameter

--- helpers/ssh-docker-compose/rebuild-and-up.sh
+++ helpers/ssh-docker-compose/rebuild-and-up.sh
@@ -4,14 +4,15 @@ set -eu
 # ${1} for Server IP
 # ${2} for Server Port
 # ${3} for Server User
+# ${4} for Dockerfile location
 
 server_ip="${1}"
 server_port="${2}"
 server_user="${3}"
-
+docker_compose_file="${4}"
 
 ssh -i priv.key -o BatchMode=yes \
     -p "${server_port}" \
     -o UserKnownHostsFile=/dev/null \
     -o StrictHostKeyChecking=no \
-    "${server_user}"@"${server_ip}" "cd src/ && docker compose build --no-cache && docker compose stop && docker compose up -d"
+    "${server_user}"@"${server_ip}" "cd src/ && docker compose -f ${docker_compose_file} build --no-cache && docker compose -f ${docker_compose_file} stop && docker compose -f ${docker_compose_file} up -d"
