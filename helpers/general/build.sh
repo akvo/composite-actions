dec0af3ac6b18e797d7ce0447ed0f1f267299199
Anjar Fiandrianto <anjar@akvo.org>
2024-09-27 13:57:43 +0700

    Add cluster name variable on docker build

--- helpers/general/build.sh
+++ helpers/general/build.sh
@@ -14,20 +14,20 @@ image_prefix="${registry}/${gcloud_project}/${1}"
 service_name="${2}"
 image_version="${3}"
 dockerfile_location="${4}"
-
+cluster_name="${5}"
 
 image_build () {
     # Check if dockerfile_location is a directory
     if [ -d "${dockerfile_location}" ]; then
         # If it's a directory, change to that directory and build using the default Dockerfile
         cd "${dockerfile_location}" && docker build \
-            --tag "${image_prefix}/${service_name}:latest-test" \
+            --tag "${image_prefix}/${service_name}:latest-{cluster_name}" \
             --tag "${image_prefix}/${service_name}:${image_version}" .
     elif [ -f "${dockerfile_location}" ]; then
         # If it's a specific Dockerfile, change to the directory containing the Dockerfile
         dockerfile_dir="$(dirname "${dockerfile_location}")"
         dockerfile_name="$(basename "${dockerfile_location}")"
-        cd "${dockerfile_dir}" && docker build --tag "${image_prefix}/${service_name}:latest-test" \
+        cd "${dockerfile_dir}" && docker build --tag "${image_prefix}/${service_name}:latest-{cluster_name}" \
                                                   --tag "${image_prefix}/${service_name}:${image_version}" \
                                                   -f "${dockerfile_name}" .
     else
